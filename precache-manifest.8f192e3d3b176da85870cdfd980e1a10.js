f2a9d185a796afc45f2566e254c484703e4b6af5
domaradzkiJs <56085891+domaradzkiJs@users.noreply.github.com>
2019-10-15 14:14:46 +0200

    Updates

---
+++ precache-manifest.8f192e3d3b176da85870cdfd980e1a10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbb77e3e986fb3b4f29f468d5037329",
+    "url": "/sweet-recipes/index.html"
+  },
+  {
+    "revision": "793834aa648650b4767c",
+    "url": "/sweet-recipes/static/css/main.f41a935e.chunk.css"
+  },
+  {
+    "revision": "213447ea3fcc3c0217ba",
+    "url": "/sweet-recipes/static/js/2.f6fa5f26.chunk.js"
+  },
+  {
+    "revision": "793834aa648650b4767c",
+    "url": "/sweet-recipes/static/js/main.d3f519e9.chunk.js"
+  },
+  {
+    "revision": "6044c6beebf25c1c8fce",
+    "url": "/sweet-recipes/static/js/runtime-main.a09c8abb.js"
+  },
+  {
+    "revision": "4e6ac015bc475f103a38bb00ea7e704f",
+    "url": "/sweet-recipes/static/media/donut.4e6ac015.svg"
+  },
+  {
+    "revision": "d654ce194e0660d047d6913124f8d562",
+    "url": "/sweet-recipes/static/media/like.d654ce19.svg"
+  },
+  {
+    "revision": "b53323c3e037d9178451d86493f2f373",
+    "url": "/sweet-recipes/static/media/search.b53323c3.svg"
+  }
+]);
